87a25ed0aa3fc9cd9722a561fbbf82514e9d2569
kazupong <kzpng2@gmail.com>
2018-06-15 18:30:48 -0700

    Initial Commit

---
+++ Annotation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Annotation
+//
+//  Created by Kazuyuki Nakatsu on 6/15/18.
+//  Copyright © 2018 Kazuyuki Nakatsu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Annotation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Annotation
+//
+//  Created by Kazuyuki Nakatsu on 6/15/18.
+//  Copyright © 2018 Kazuyuki Nakatsu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
